f3c8e3b330456e1912a2761a736f8036877a2c05
dongmonash <147219745+dongmonash@users.noreply.github.com>
2023-10-08 18:43:46 +1100

    Add files via upload

---
+++ vega_lite_map.js
@@ -0,0 +1,4 @@
+var vg_1 = "FIT_VS2_map_draftout.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
