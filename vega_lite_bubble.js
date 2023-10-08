cc002a317b96f7fd15ff1b146d9a8852725f8d54
dongmonash <147219745+dongmonash@users.noreply.github.com>
2023-10-08 18:43:31 +1100

    Add files via upload

---
+++ vega_lite_bubble.js
@@ -0,0 +1,4 @@
+var vg_1 = "FIT_VS2_bubble_draftout.json";
+vegaEmbed("#bubble", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
