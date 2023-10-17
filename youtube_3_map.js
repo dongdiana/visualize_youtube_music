d50b7dcf1eb3bb5815286bcbfb094a5a08a0d0f4
dongmonash <147219745+dongmonash@users.noreply.github.com>
2023-10-17 21:38:47 +1100

    Add files via upload

---
+++ youtube_3_map.js
@@ -0,0 +1,4 @@
+var vg_1 = "youtube_3_map.json";
+vegaEmbed("#youtube3", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
